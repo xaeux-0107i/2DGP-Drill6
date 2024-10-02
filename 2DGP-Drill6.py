8bdda7eff8319a7e6d2179026e5fffc76e024fc3
xaeux0107 <ekdms1324@gmail.com>
2024-10-02 23:44:00 +0900

    창 닫기 기능 추가

--- 2DGP-Drill6.py
+++ 2DGP-Drill6.py
@@ -1,6 +1,15 @@
 from pico2d import *
 from random import *
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.type == SDLK_ESCAPE:
+            running = False
+
 open_canvas()
 
 ground = load_image('TUK_GROUND.png')
@@ -15,10 +24,12 @@ hx = randint(1, 800)
 hy = randint(1, 600)
 frame = 0
 
-ground.clip_draw(0, 0, 1280, 1024, 800//2, 600//2, 800, 600)
-character.clip_draw(frame*100, 100, x, y, 100, 100)
-hand.clip_draw(0, 0, 50, 52, hx, hy, 40, 40)
-update_canvas()
-frame = (frame + 1) % 8
+while running:
+    ground.clip_draw(0, 0, 1280, 1024, 800//2, 600//2, 800, 600)
+    character.clip_draw(frame*100, 100, x, y, 100, 100)
+    hand.clip_draw(0, 0, 50, 52, hx, hy, 40, 40)
+    update_canvas()
+    frame = (frame + 1) % 8
+    handle_events()
 
-delay(1)
+close_canvas()
